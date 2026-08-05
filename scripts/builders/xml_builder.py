d5d06e93a58b5eaa3eb07d2325c01be65c28fb01
Chefferson <shayankasaei@gmail.com>
2026-08-05 23:05:45 +0330

    Update base URL in xml_builder.py

--- scripts/builders/xml_builder.py
+++ scripts/builders/xml_builder.py
@@ -35,7 +35,7 @@ class XMLBuilder:
         base_url = (
             "https://htmlpreview.github.io/?"
             "https://raw.githubusercontent.com/"
-            "shawnkasaei/news-reader-meli/"
+            "shawnkasaei/dayereh/"
             "refs/heads/main/feeds/view/index.html"
         )
 
